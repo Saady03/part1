0ec66c3884b5d49899abab5040b57dc738ac8c11
Saadath <saadathmama@gmail.com>
2024-11-28 15:08:39 +0100

    Init part1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  const friends = [
+    'Peter', 'Maya'
+  ]
+  const a = 10
+  const b = 20
+  return (
+  <div>
+    <p>{friends}</p>
+  </div>
+  )
+}
+
+export default App
